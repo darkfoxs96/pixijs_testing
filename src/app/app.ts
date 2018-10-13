611e9290bed34b88dd4bf18c91c71e364fdf958c
darkfoxs96 <35303397+darkfoxs96@users.noreply.github.com>
2018-10-13 13:52:08 +0300

    Create app.ts

---
+++ src/app/app.ts
@@ -0,0 +1,7 @@
+import * as PIXI from 'pixi.js';
+import {User} from "./models/user.model";
+
+let q = new User();
+const sd = new PIXI.filters.BlurFilter();
+console.log(q);
+console.log('cucu =>>>', sd);
